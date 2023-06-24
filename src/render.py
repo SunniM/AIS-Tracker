551db15d6735099771be6fbec6fa8c6586210910
SunniM <sunnismagan@gmail.com>
2023-06-23 20:45:50 -0500

    remove redundent code

--- src/render.py
+++ src/render.py
@@ -29,27 +29,5 @@ def render(width, height):
 
     pygame.quit()
 
-    # # Create the main window
-    # window = Tk()
-
-    # # Set the window title
-    # window.title("Image Background Example")
-
-    # # Set the window size
-    # window.geometry("1920x1080")
-
-    # # Load the image
-    # image = Image.open("map_image.jpg")
-    # background_image = ImageTk.PhotoImage(image)
-
-    # # Create a label with the image as the background
-    # background_label = Label(window, image=background_image)
-    # background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-    # # Add other widgets or perform other operations on the window
-
-    # # Start the main event loop
-    # window.mainloop()
-
 if __name__ == '__main__':
     render()
