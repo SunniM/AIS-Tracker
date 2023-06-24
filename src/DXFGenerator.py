6c08f4ec0719897300dbde1ad0361c49323cdec4
Tan560 <97492534+Tan560@users.noreply.github.com>
2023-06-24 11:13:05 -0400

    Created DXF file based on size and added border

--- src/DXFGenerator.py
+++ src/DXFGenerator.py
@@ -2,6 +2,48 @@ import cv2
 import numpy as np
 import ezdxf
 
+def generate_dxf_from_contours(contours, width, height, border_size, output_file):
+    if len(contours) == 0:
+        return  # Exit if no contours are provided
+
+    # Create a DXF document
+    doc = ezdxf.new('R2010')  # Use the DXF R2010 format
+
+    # Create a new layer for the contours
+    contour_layer = doc.layers.new(name='Contours')
+
+    # Get the model space of the document
+    msp = doc.modelspace()
+
+    # Calculate the border coordinates
+    border_coords = [
+        (border_size, border_size),
+        (width - border_size, border_size),
+        (width - border_size, height - border_size),
+        (border_size, height - border_size),
+        (border_size, border_size)
+    ]
+
+    # Add the border as a polyline entity to the model space
+    msp.add_lwpolyline(border_coords, dxfattribs={'layer': 'Contours', 'closed': True})
+
+    # Scale and flip the contour points for each contour
+    for contour in contours:
+        if len(contour) > 0:
+            # Scale the contour points based on the desired size
+            scale_x = (width - 2 * border_size) / float(image.shape[1])
+            scale_y = (height - 2 * border_size) / float(image.shape[0])
+            scaled_contour = contour[:, 0, :] * np.array([scale_x, scale_y])
+
+            # Flip the y-coordinates of the scaled contour points
+            flipped_contour = np.array([(point[0] + border_size, height - point[1] - border_size) for point in scaled_contour])
+
+            # Create a new polyline entity for the contour and add it to the model space
+            msp.add_lwpolyline(list(flipped_contour), dxfattribs={'layer': 'Contours'})
+
+    # Save the DXF file
+    doc.saveas(output_file)
+
 # Load the image
 image = cv2.imread('d:\Programming Projects\C++\AIS Tracker\AIS-Tracker\src\map_image.jpg', cv2.IMREAD_UNCHANGED)
 
@@ -14,21 +56,11 @@ ret, thresh_img = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)
 # Find contours
 contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-# Create a DXF document
-doc = ezdxf.new('R2010')  # Use the DXF R2010 format
+# Specify the desired size of the DXF drawing and the border size
+width = 610  # Width in units (e.g., millimeters)
+height = 432  # Height in units (e.g., millimeters)
+border_size = 2.0  # Border size in units (e.g., millimeters)
 
-# Create a new layer for the contours
-contour_layer = doc.layers.new(name='Contours')
-
-# Get the model space of the document
-msp = doc.modelspace()
-
-# Create a new polyline entity for each contour and add it to the model space
-for contour in contours:
-    points = [tuple(point[0]) for point in contour]
-    flipped_points = [(x, image.shape[0] - y) for x, y in points]  # Flip the y-coordinates
-    flipped_points.append(flipped_points[0])  # Repeat the first point to close the polyline
-    msp.add_lwpolyline(flipped_points, dxfattribs={'layer': 'Contours'})
-
-# Save the DXF file
-doc.saveas('contours.dxf')
+# Generate a single DXF file for all contours with a border
+output_file = 'contours.dxf'
+generate_dxf_from_contours(contours, width, height, border_size, output_file)
