62be747240f981c2cdeef4a3ad9518b69f5e21f9
SunniM <sunnismagan@gmail.com>
2023-06-26 02:14:24 -0500

    sends image_data to main to post event

--- src/main.py
+++ src/main.py
@@ -1,33 +1,32 @@
 
 import Map
 import multiprocessing as mp
-from tkinter import Tk, Label
-from PIL import Image, ImageTk
 import pygame
 
-import Server, WebSocketHandler, render
+import Server, WebSocketHandler, render, events
 
 
 server_address = ('localhost', 8080)
-new_image_event = pygame.USEREVENT + 1
-
 
 def main():
     # pipe used by server and controller
     server_pipe, controller_pipe = mp.Pipe()
+    image_queue = mp.Queue()
 
     ws_handler = None
-    window_process = mp.Process(target=render.render, args=(1920,1080))
+    window_process = mp.Process(target=render.render, args=(1920, 1080, image_queue))
     window_process.start()
 
     # Starting local webserver
-    server_process = mp.Process(target=Server.run_server, args=(server_pipe,))
+    server_process = mp.Process(target=Server.run_server, args=(server_pipe,image_queue))
     server_process.start()
 
     while True:
         # checks for incoming messages from server
         if controller_pipe.poll():
             data = controller_pipe.recv()
+            print('data received')
+
 
             # parses message
             match type(data):
@@ -47,12 +46,20 @@ def main():
                     # ws_handler = WebSocketHandler.WebSocketHandler(
                     #     south, west, north, east)
                     # ws_handler.start()
-
+                    if not window_process.is_alive():
+                        window_process.start()
+                        window_process.join()
+                case bytes:
+                    print('bytes received')
+                    image_queue.put(data)
+                    event = pygame.event.Event(events.NEW_IMAGE_EVENT)
                     if not window_process.is_alive():
                         window_process.start()
                         window_process.join()
                     else:
-                        pygame.event.post(new_image_event)
+                        pygame.event.post(event)
+
+
 
 if __name__ == '__main__':
     main()
--- src/render.py
+++ src/render.py
@@ -1,26 +1,41 @@
 import pygame
+from io import BytesIO
 
-def render(width, height):
+import events
+def render(width, height, image_queue=None):
+
     image = pygame.image.load("map_image.jpg")
-    pygame.init()
+    # pygame.init()
+    print('video system intiailzed')
     screen = pygame.display.set_mode((width, height))
     clock = pygame.time.Clock()
 
     running = True
+    print('Original Value:', events.NEW_IMAGE_EVENT)
     while running:
+
         for event in pygame.event.get():
+            print(event)
             match event.type:
                 case pygame.QUIT:
                     running = False
-                case new_image_event:
-                    image = pygame.image.load("map_image.jpg")
+                case events.NEW_IMAGE_EVENT:
+                    print('new_image_recieved')
+                    if image_queue:
+                        image = pygame.image.frombytes(image_queue.get())
+                        
 
         screen.fill((0, 0, 0))  # Clear the screen
 
         # Load the current image based on the current_image_index
 
-        # Use the loaded image within the loop
-        screen.blit(image, (0, 0))
+        # Calculate the position to center the image
+        image_rect = image.get_rect()
+        x = (width - image_rect.width) // 2
+        y = (height - image_rect.height) // 2
+
+        # Blit the image at the calculated position
+        screen.blit(image, (x, y))
 
         pygame.display.flip()  # Update the screen
         clock.tick(60)  # Limit the frame rate
@@ -30,4 +45,4 @@ def render(width, height):
     pygame.quit()
 
 if __name__ == '__main__':
-    render()
+    render(1920, 1080)
