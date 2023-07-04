f92861e91f988f9c915eeca0a85f4a6201b6c7ec
SunniM <sunnismagan@gmail.com>
2023-07-03 22:15:40 -0500

    add ship event implement map resolution

--- src/WebSocketHandler.py
+++ src/WebSocketHandler.py
@@ -1,36 +1,51 @@
-import websocket, threading, time, rel, json
+import websocket
+import threading
+import time
+import rel
+import json
 from datetime import datetime, timezone
 
+
 class WebSocketHandler():
-    def __init__(self, south, west, north, east, queue=None):
-        self.queue = queue
+    def __init__(self, input_queue=None, output_queue=None, map=None):
+        self.output_queue = output_queue
+        self.input_queue = input_queue
+        self.map = map
+
+        self.ws = websocket.WebSocketApp("wss://stream.aisstream.io/v0/stream",
+                                         on_open=self.on_open,
+                                         on_message=self.on_message,
+                                         on_error=self.on_error,
+                                         on_close=self.on_close,
+                                         )
+
+    def run(self, south, west, north, east):
         self.south = south
         self.west = west
         self.north = north
         self.east = east
-        self.ws = websocket.WebSocketApp("wss://stream.aisstream.io/v0/stream",
-                                     on_open=self.on_open,
-                                     on_message=self.on_message,
-                                     on_error=self.on_error,
-                                     on_close=self.on_close,
-                                     )
-        
-    def run(self):
-        # websocket.enableTrace(True)
-        queue_thread = threading.Thread(target=self.check_queue)
-        queue_thread.start()
 
-        self.ws.run_forever(dispatcher=rel) # dispatcher = rel
-        rel.signal(2, rel.abort)  # Keyboard Interrupt  
-        rel.dispatch() 
+        if self.input_queue:
+            queue_thread = threading.Thread(target=self.check_queue)
+            queue_thread.start()
+
+        self.ws.run_forever(dispatcher=rel)  # dispatcher = rel
+        rel.signal(2, rel.abort)  # Keyboard Interrupt
+        rel.dispatch()
+        queue_thread.join()
 
     def check_queue(self):
         while True:
-            if not self.queue.empty():
-                subscription_message = self.queue.get()
+                subscription_message = self.input_queue.get()
                 self.ws.send(subscription_message)
-                print("Resubscribed")
+                render_message = {
+                    "message_type": "clear_list",
+                    "message": ""
+                }
+                render_message = json.dumps(render_message)
+                self.output_queue.put(render_message)
 
+                
 
     def on_message(self, ws, message):
         message = json.loads(message)
@@ -39,22 +54,34 @@ class WebSocketHandler():
         if message_type == "PositionReport":
             # the message parameter contains a key of the message type which contains the message itself
             ais_message = message['Message']['PositionReport']
-            
+
             Latitude = ais_message['Latitude']
             Longitude = ais_message['Longitude']
-            
-            #should be in a methode, but wasn't able to get it working
+
+            # should be in a method, but wasn't able to get it working
             width = 1920
             height = 1080
-            
-            #0,0 is north west of the bounding box
-            #calculating a ratio for the x and y pixel based on size of the window
+
+            # 0,0 is north west of the bounding box
+            # calculating a ratio for the x and y pixel based on size of the window
             x = int(width * abs((Longitude - self.west) / (self.east - self.west)))
             y = int(height * abs((self.north - Latitude) / (self.north - self.south)))
- 
+
             # end of should be in a methode, but wasn't able to get it working
-            
-            print(f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Latitude: {Latitude} Longitude: {Longitude} x Pixel: {x} y Pixel: {y}")
+            ship_message = {
+                "message_type": "ship_data",
+                "message": {
+                    "ship_id": ais_message['UserID'],
+                    "x": x,
+                    "y": y}
+            }
+            ship_message = json.dumps(ship_message)
+            if self.output_queue:
+                self.output_queue.put(ship_message)
+            else:
+                print(ship_message)
+
+            # print(f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Latitude: {Latitude} Longitude: {Longitude} x Pixel: {x} y Pixel: {y}")
 
     def on_error(self, ws, error):
         print(error)
@@ -64,7 +91,7 @@ class WebSocketHandler():
         print("Connection Closed")
         print('status code: ', close_status_code)
         print('message: ', close_msg)
-
+        self.run()
 
     def on_open(self, ws):
         self.subscribe()
@@ -88,15 +115,17 @@ class WebSocketHandler():
         self.subscribe()
 
     def subscribe(self):
-        subscription_message = {"APIKey": "d77b1be3c710d2d404386475ef886b33989950e3", "BoundingBoxes": [[[self.south, self.west], [self.north, self.east]]]}
+        subscription_message = {"APIKey": "d77b1be3c710d2d404386475ef886b33989950e3", "BoundingBoxes": [
+            [[self.south, self.west], [self.north, self.east]]]}
         json_message = json.dumps(subscription_message)
         self.ws.send(json_message)
 
     def make_subscription_message(south, west, north, east):
-        subscription_message = {"APIKey": "d77b1be3c710d2d404386475ef886b33989950e3", "BoundingBoxes": [[[south, west], [north, east]]]}
+        subscription_message = {"APIKey": "d77b1be3c710d2d404386475ef886b33989950e3", "BoundingBoxes": [
+            [[south, west], [north, east]]]}
         return json.dumps(subscription_message)
 
 
 if __name__ == '__main__':
-    conn = WebSocketHandler(None, -90, -180, 90, 180)
-    conn.run()
+    conn = WebSocketHandler()
+    conn.run(-90, -180, 90, 180)
